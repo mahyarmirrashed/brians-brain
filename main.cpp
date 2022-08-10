74d323400dc087263f318c4c604b2715a6449820
Mahyar Mirrashed <mah.mirr@gmail.com>
2022-08-10 17:39:57 -0500

    feat: add more argument parser options
    Allow control over number of frames and resolution of outputted video.

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
  * @brief Generate frames from the Brian's Brain cellular automaton.
  * @version 0.0.0
  * @date 2022-08-10
- * 
+ *
  * @copyright Copyright (c) 2022 Mahyar Mirrashed
- * 
+ *
  */
 
 #include <argp.h>
@@ -18,6 +18,27 @@
 
 const char *argp_program_version = "brains-brain 0.0.0";
 const char *argp_program_bug_address = "<mirrashm@myumanitoba.ca>";
+
+static char doc[] = "Brian's Brian cellular automaton video generator.";
+
+static struct argp_option options[] = {
+  {.name = "frames",
+   .key = 'f',
+   .arg = "FRAMES",
+   .flags = OPTION_ARG_OPTIONAL,
+   .doc = "Number of frames to generate"},
+  {.name = "rows",
+   .key = 'r',
+   .arg = "ROWS",
+   .flags = OPTION_ARG_OPTIONAL,
+   .doc = "Number of rows in each frame"},
+  {.name = "columns",
+   .key = 'c',
+   .arg = "COLUMNS",
+   .flags = OPTION_ARG_OPTIONAL,
+   .doc = "Number of columns in each frame"},
+  {0},
+};
 
 //-----------------------------------------------------------------------------
 // PROTOTYPES
