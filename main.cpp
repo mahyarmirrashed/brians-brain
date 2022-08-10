7c94fbcf306c964d517adf8710fd5b97843b1882
Mahyar Mirrashed <mah.mirr@gmail.com>
2022-08-10 17:33:49 -0500

    feat: add basic program outline

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file main.cpp
+ * @author Mahyar Mirrashed (mirrashm@myumanitoba.ca)
+ * @brief Generate frames from the Brian's Brain cellular automaton.
+ * @version 0.0.0
+ * @date 2022-08-10
+ * 
+ * @copyright Copyright (c) 2022 Mahyar Mirrashed
+ * 
+ */
+
+#include <argp.h>
+#include <stdlib.h>
+
+//-----------------------------------------------------------------------------
+// ARGUMENT PARSER SETUP
+//-----------------------------------------------------------------------------
+
+const char *argp_program_version = "brains-brain 0.0.0";
+const char *argp_program_bug_address = "<mirrashm@myumanitoba.ca>";
+
+//-----------------------------------------------------------------------------
+// PROTOTYPES
+//-----------------------------------------------------------------------------
+
+int main(const int argc, const char **argv) {
+  return EXIT_SUCCESS;
+}
