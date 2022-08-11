0089eb787f605f7121f663f7e049121fb5e1b6d0
Mahyar Mirrashed <mah.mirr@gmail.com>
2022-08-11 00:07:47 -0500

    feat: add brains brain automaton function

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
  * @file main.cpp
  * @author Mahyar Mirrashed (mirrashm@myumanitoba.ca)
  * @brief Generate frames from the Brian's Brain cellular automaton.
- * @version 0.0.0
+ * @version 0.1.0
  * @date 2022-08-10
  *
  * @copyright Copyright (c) 2022 Mahyar Mirrashed
@@ -14,6 +14,8 @@
 #include <limits.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
 
 //-----------------------------------------------------------------------------
 // CONSTANTS
@@ -24,6 +26,10 @@
 // default standard high-definition (HD) display resolution (720p)
 #define DEFAULT_COLUMNS 1280
 #define DEFAULT_ROWS    720
+
+static const cv::Vec3b ON = cv::Vec3b({255, 255, 255});
+static const cv::Vec3b DYING = cv::Vec3b({255, 0, 0});
+static const cv::Vec3b OFF = cv::Vec3b({0, 0, 0});
 
 //-----------------------------------------------------------------------------
 // ARGUMENT PARSER SETUP
@@ -67,6 +73,7 @@ static arguments args;
 //-----------------------------------------------------------------------------
 
 int main(int argc, char **argv);
+static void brain(const cv::Mat &__restrict__ in, cv::Mat &__restrict__ out);
 static error_t parse_opt(int key, char *arg, struct argp_state *state);
 
 //-----------------------------------------------------------------------------
@@ -90,6 +97,11 @@ static struct argp argp {
  * @return int Return code status of program.
  */
 int main(int argc, char **argv) {
+  // variables used for video generation
+  int i, j;
+  cv::VideoWriter video;
+  cv::Mat prev, curr;
+
   // set default argument values
   args.frames = DEFAULT_FRAME_COUNT;
   args.columns = DEFAULT_COLUMNS;
@@ -98,7 +110,59 @@ int main(int argc, char **argv) {
   // parse arguments from argument vector
   argp_parse(&argp, argc, argv, 0, 0, &args);
 
+  // create video stream (receives bytes from input matrix)
+  video.open(
+    "automaton.avi", cv::VideoWriter::fourcc('F', 'F', 'V', '1'), 30.0,
+    cv::Size(args.columns, args.rows)
+  );
+
+  // video frame size initialization
+  prev = cv::Mat(args.rows, args.columns, CV_8UC3);
+  curr = cv::Mat(args.rows, args.columns, CV_8UC3);
+
+  // randomly seed frame for interesting initialization
+  // TODO
+
+  for (i = 0; i < args.frames; i += 1) {
+    // save current frame
+    video << curr;
+    // migrate current frame to previous frame
+    curr.copyTo(prev);
+    // calculate next frame of automaton
+    brain(prev, curr);
+  }
+
   return EXIT_SUCCESS;
+}
+
+/**
+ * @brief Model of Brian's Brain implemented as cleanly as possible.
+ *
+ * @param in Previous frame of Brian's Brain.
+ * @param out New, current frame of Brian's Brain.
+ */
+static void brain(const cv::Mat &__restrict__ in, cv::Mat &__restrict__ out) {
+  int i, j, k, l;
+  int tot;
+
+  for (i = 0; i < in.rows; i += 1)
+    for (j = 0; j < in.cols; j += 1)
+      if (in.at<cv::Vec3b>(i, j) == ON)
+        out.at<cv::Vec3b>(i, j) = DYING;
+      else if (in.at<cv::Vec3b>(i, j) == DYING)
+        out.at<cv::Vec3b>(i, j) = OFF;
+      else {
+        // search Moore neighborhood for live cells
+        for (tot = 0, k = -1; k < 2; k += 1)
+          if (i + k >= 0 && i + k < in.rows)
+            for (l = -1; l < 2; l += 1)
+              if (j + l >= 0 && j + l < in.cols)
+                if (in.at<cv::Vec3b>(i + k, j + l) == ON)
+                  tot += 1;
+
+        // automaton rule dictates turning on only if two neighbor cells are on
+        out.at<cv::Vec3b>(i, j) = (tot == 2) ? ON : OFF;
+      }
 }
 
 /**
